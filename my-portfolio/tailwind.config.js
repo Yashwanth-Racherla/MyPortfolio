f4feb6b4db1c4dba0191e40c8d27a137f5c04e73
Rohith Racherla <rohith.racherla@steinias.com>
2022-06-12 15:30:12 +0530

    created react app and installed tailwind and react router dom

---
+++ my-portfolio/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+        screens: {
+      'ms': '400px',
+      'ts': '500px',
+      'sm': '576px',
+      'md': '768px',
+      'lg': '950px',
+      'xl': '1200px',
+      '2xl': '1400px',      
+   },
+  },
+  plugins: [],
+}
