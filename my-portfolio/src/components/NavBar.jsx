3250c5db8059cac130a9935d8058b9d093bbad18
Rohith Racherla <rohith.racherla@steinias.com>
2022-06-12 17:06:28 +0530

    nav bar html and css modified

--- my-portfolio/src/components/NavBar.jsx
+++ my-portfolio/src/components/NavBar.jsx
@@ -3,38 +3,29 @@ import React, { useState } from "react";
 const NavBar = () => {
   const [toggle, setToggle] = useState(true);
 
-  const toggleIcon = () => {
-    setToggle(!toggle);
-    document.querySelector(".nav-links-mobile").classList.toggle("active");
-  };
-
   return (
-    <header className="nav-menu">
-      <div className="portfolio-name">YASHWANTH RACHERLA</div>
+    <header className="nav-bar">
+      <div className="nav-bar-title">YASHWANTH RACHERLA</div>
 
-      <button className="toggle-button" onClick={() => toggleIcon()}>
-        {toggle ? (
-          <img src="./menu.png" alt="toggle-icon" />
-        ) : (
-          <img src="./close.png" alt="close-icon" />
-        )}
-      </button>
+      <div>
+        <button className="toggle-button" onClick={() => setToggle(!toggle)}>
+          {toggle ? (
+            <img src="./menu.png" alt="toggle-icon" />
+          ) : (
+            <img src="./close.png" alt="close-icon" />
+          )}
+        </button>
+      </div>
 
-      <ul className="nav-links">
-        <li className="nav-link">Home</li>
-        <li className="nav-link">About</li>
-        <li className="nav-link">Services</li>
-        <li className="nav-link">Portfolio</li>
-        <li className="nav-link">Contact</li>
-      </ul>
-
-      <ul className="nav-links-mobile">
-        <li className="nav-link-mobile">Home</li>
-        <li className="nav-link-mobile">About</li>
-        <li className="nav-link-mobile">Services</li>
-        <li className="nav-link-mobile">Portfolio</li>
-        <li className="nav-link-mobile">Contact</li>
-      </ul>
+      <div className={`nav-menu ${!toggle && "mobile"}`}>
+        <ul className="nav-links">
+          <li className="nav-link">Home</li>
+          <li className="nav-link">About</li>
+          <li className="nav-link">Services</li>
+          <li className="nav-link">Portfolio</li>
+          <li className="nav-link">Contact</li>
+        </ul>
+      </div>
     </header>
   );
 };
