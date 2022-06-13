103c4e60d58a690d04b000f47cd6b194b70ff95b
Rohith Racherla <rohith.racherla@steinias.com>
2022-06-13 09:16:11 +0530

    footer section html css part complete

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Footer />
     </>
   );
 }
