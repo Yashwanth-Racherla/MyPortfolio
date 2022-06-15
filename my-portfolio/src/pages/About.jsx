61f155cacc7f495819b545b7e978262334b2fec0
Rohith Racherla <rohith.racherla@steinias.com>
2022-06-15 13:29:07 +0530

    Biodata section wip

--- my-portfolio/src/pages/About.jsx
+++ my-portfolio/src/pages/About.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="section biodata-section">
+      <div className="container">
+        <div>
+          <h2 className="section-title">My Biodata</h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore autem
+          iure temporibus nihil adipisci eius tempora perferendis aut
+          consequuntur doloremque fugiat repudiandae magnam saepe cum, quos
+          sapiente quibusdam a enim!
+        </div>
+
+        {/* <div className="p-6">
+          <img
+            className="w-full"
+            src="./about-page-image.jpg"
+            alt="biodata-img"
+          />
+        </div> */}
+      </div>
+    </div>
+  );
 };
 
 export default About;
