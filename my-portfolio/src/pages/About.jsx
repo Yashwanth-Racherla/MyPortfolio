7612c4d0e44a148960707e3ae8401b88509f9239
Rohith Racherla <rohith.racherla@steinias.com>
2022-06-15 17:43:26 +0530

    About section mobile screen finished

--- my-portfolio/src/pages/About.jsx
+++ my-portfolio/src/pages/About.jsx
@@ -17,34 +17,47 @@ const About = () => {
             molestias?
           </p>
         </div>
-        <div className="my-skills">
-          <h3 className="progress-bar">HTML</h3>
-          <span className="skill-bar">95%</span>
+        <div className="mb-16">
+          <div className="biodata-stats">
+            <h3 className="biodata-progress-bar">HTML</h3>
+            <span className="biodata-skill-bar">95%</span>
+          </div>
+          <div className="biodata-stats">
+            <h3 className="biodata-progress-bar biodata-progress-bar-2">CSS</h3>
+            <span className="biodata-skill-bar biodata-skill-bar-2">90%</span>
+          </div>
+          <div className="biodata-stats">
+            <h3 className="biodata-progress-bar biodata-progress-bar-3">
+              JavaScript
+            </h3>
+            <span className="biodata-skill-bar biodata-skill-bar-3">80%</span>
+          </div>
+          <div className="biodata-stats">
+            <h3 className="biodata-progress-bar biodata-progress-bar-4">
+              React
+            </h3>
+            <span className="biodata-skill-bar biodata-skill-bar-4">70%</span>
+          </div>
+          <div className="biodata-stats">
+            <h3 className="biodata-progress-bar biodata-progress-bar-5">
+              JQuery
+            </h3>
+            <span className="biodata-skill-bar biodata-skill-bar-5">85%</span>
+          </div>
         </div>
-        <div className="my-skills">
-          <h3 className="progress-bar progress-bar-2">CSS</h3>
-          <span className="skill-bar skill-bar-2">90%</span>
-        </div>
-        <div className="my-skills">
-          <h3 className="progress-bar progress-bar-3">JavaScript</h3>
-          <span className="skill-bar skill-bar-3">80%</span>
-        </div>
-        <div className="my-skills">
-          <h3 className="progress-bar progress-bar-4">React</h3>
-          <span className="skill-bar skill-bar-4">70%</span>
-        </div>
-        <div className="my-skills">
-          <h3 className="progress-bar progress-bar-5">JQuery</h3>
-          <span className="skill-bar skill-bar-5">85%</span>
+        <div>
+          <button className="button">
+            <a href="/about">Download Resume</a>
+          </button>
         </div>
 
-        {/* <div className="p-6">
+        <div className="py-10">
           <img
-            className="w-full"
+            className="biodata-image"
             src="./about-page-image.jpg"
             alt="biodata-img"
           />
-        </div> */}
+        </div>
       </div>
     </div>
   );
