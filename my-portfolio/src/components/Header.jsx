9485d5021a5f18556ee24ee205c2df83103c4c9e
Rohith Racherla <rohith.racherla@steinias.com>
2022-06-15 13:28:38 +0530

    header section class names changed  and wrapped under an extra div

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -5,37 +5,37 @@ const Header = () => {
   const [toggle, setToggle] = useState(true);
 
   return (
-    <header className="nav-bar">
-      <div className="nav-bar-title">YASHWANTH RACHERLA</div>
-
-      <div>
-        <button className="toggle-button" onClick={() => setToggle(!toggle)}>
-          {toggle ? (
-            <img src="./menu.png" alt="toggle-icon" />
-          ) : (
-            <img src="./close.png" alt="close-icon" />
-          )}
-        </button>
-      </div>
-
-      <div className={`nav-menu ${!toggle && "mobile"}`}>
-        <ul className="nav-links">
-          <li className="nav-link" onClick={() => setToggle(!toggle)}>
-            <Link to="/">Home</Link>
-          </li>
-          <li className="nav-link" onClick={() => setToggle(!toggle)}>
-            <Link to="/About">About</Link>
-          </li>
-          <li className="nav-link" onClick={() => setToggle(!toggle)}>
-            <Link to="/Services">Services</Link>
-          </li>
-          <li className="nav-link" onClick={() => setToggle(!toggle)}>
-            <Link to="/Portfolio">Portfolio</Link>
-          </li>
-          <li className="nav-link" onClick={() => setToggle(!toggle)}>
-            <Link to="/Contact">Contact</Link>
-          </li>
-        </ul>
+    <header className="header-section">
+      <div className="nav-bar container">
+        <div className="nav-bar-title">YASHWANTH RACHERLA</div>
+        <div>
+          <button className="toggle-button" onClick={() => setToggle(!toggle)}>
+            {toggle ? (
+              <img src="./menu.png" alt="toggle-icon" />
+            ) : (
+              <img src="./close.png" alt="close-icon" />
+            )}
+          </button>
+        </div>
+        <div className={`nav-menu ${!toggle && "mobile"}`}>
+          <ul className="nav-links">
+            <li className="nav-link" onClick={() => setToggle(!toggle)}>
+              <Link to="/">Home</Link>
+            </li>
+            <li className="nav-link" onClick={() => setToggle(!toggle)}>
+              <Link to="/About">About</Link>
+            </li>
+            <li className="nav-link" onClick={() => setToggle(!toggle)}>
+              <Link to="/Services">Services</Link>
+            </li>
+            <li className="nav-link" onClick={() => setToggle(!toggle)}>
+              <Link to="/Portfolio">Portfolio</Link>
+            </li>
+            <li className="nav-link" onClick={() => setToggle(!toggle)}>
+              <Link to="/Contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
