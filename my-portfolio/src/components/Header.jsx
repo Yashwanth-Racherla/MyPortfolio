a791972468d63a39d0a5956badb43a8de345911e
Yashwanth Racherla <ignitingyash@gmail.com>
2022-06-27 18:16:00 +0530

    header section - mobile nav bar overlflow Y issue resolved

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -4,12 +4,24 @@ import { Link } from "react-router-dom";
 const Header = () => {
   const [toggle, setToggle] = useState(true);
 
+  const setToggleFunc = (toggleValue) => {
+    setToggle(toggleValue);
+    if (!toggleValue) {
+      document.body.style.overflowY = "hidden";
+    } else {
+      document.body.style.overflowY = "scroll";
+    }
+  };
+
   return (
     <header className="header-section">
       <div className="nav-bar container">
         <div className="nav-bar-title">YASHWANTH RACHERLA</div>
         <div>
-          <button className="toggle-button" onClick={() => setToggle(!toggle)}>
+          <button
+            className="toggle-button"
+            onClick={() => setToggleFunc(!toggle)}
+          >
             {toggle ? (
               <img src="./menu.png" alt="toggle-icon" />
             ) : (
@@ -19,19 +31,19 @@ const Header = () => {
         </div>
         <div className={`nav-menu ${!toggle && "mobile"}`}>
           <ul className="nav-links">
-            <li className="nav-link" onClick={() => setToggle(!toggle)}>
+            <li className="nav-link" onClick={() => setToggleFunc(true)}>
               <Link to="/">Home</Link>
             </li>
-            <li className="nav-link" onClick={() => setToggle(!toggle)}>
+            <li className="nav-link" onClick={() => setToggleFunc(true)}>
               <Link to="/About">About</Link>
             </li>
-            <li className="nav-link" onClick={() => setToggle(!toggle)}>
+            <li className="nav-link" onClick={() => setToggleFunc(true)}>
               <Link to="/Portfolio">Portfolio</Link>
             </li>
-            <li className="nav-link" onClick={() => setToggle(!toggle)}>
+            <li className="nav-link" onClick={() => setToggleFunc(true)}>
               <Link to="/Services">Services</Link>
             </li>
-            <li className="nav-link" onClick={() => setToggle(!toggle)}>
+            <li className="nav-link" onClick={() => setToggleFunc(true)}>
               <Link to="/Contact">Contact</Link>
             </li>
           </ul>
