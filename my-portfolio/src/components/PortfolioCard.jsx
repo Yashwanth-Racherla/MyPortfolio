c24eaea165d57990e067d4069074fefcc4c21cd5
Yashwanth Racherla <ignitingyash@gmail.com>
2022-07-02 17:08:19 +0530

    Portfolio Card overlay structure revised

--- my-portfolio/src/components/PortfolioCard.jsx
+++ my-portfolio/src/components/PortfolioCard.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const PortfolioCard = ({ imageSrc, projectName, id }) => {
+const PortfolioCard = ({
+  imageSrc,
+  projectName,
+  projectInfo,
+  id,
+  Redirect,
+}) => {
   return (
     <div className={`portfolio-card portfolio-card--${id}`}>
-      <Link to="/">
-        <img
-          className="portfolio-card-image"
-          src={imageSrc}
-          alt="portfolio img"
-        />
-        <span className="portfolio-card-overlay ">{projectName}</span>
-      </Link>
+      {/* <Link to="/"> */}
+      <img
+        className="portfolio-card-image"
+        src={imageSrc}
+        alt="portfolio img"
+      />
+      <div className="portfolio-card-overlay ">
+        <div className="portfolio-card-overlay-content">
+          <h3 className="portfolio-card-overlay-title">{projectName}</h3>
+          <span className=" portfolio-card-overlay-description">
+            {projectInfo}
+          </span>
+          <Link className="font-bold text-xl mt-4 underline" to="/">
+            {Redirect}
+          </Link>
+        </div>
+      </div>
+      {/* </Link> */}
     </div>
   );
 };
--- my-portfolio/src/pages/Portfolio.jsx
+++ my-portfolio/src/pages/Portfolio.jsx
@@ -8,13 +8,7 @@ const Portfolio = () => {
       <div className="section portfolio-section">
         <div className="container">
           <div>
-            <h2 className="section-title">My Interests</h2>
-            <p className="max-w-[700px]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-              dicta sequi asperiores maxime exercitationem odit nesciunt at
-              nihil placeat, aperiam beatae possimus nostrum autem, non aut quo
-              laborum nobis.
-            </p>
+            <h2 className="section-title">My Personal Projects</h2>
           </div>
           <div className="flex justify-center gap-4 md:gap-6 lg:gap-8 my-10">
             <Button id="1" text="Sports" />
@@ -26,32 +20,34 @@ const Portfolio = () => {
             <PortfolioCard
               id="1"
               imageSrc="./cricket.jpg"
-              projectName="Project 1"
+              projectName="My Cricket Website"
+              projectInfo="Being a cricket fan, I created a website which shows live score updates."
+              Redirect="Click Here"
             />
             <PortfolioCard
               id="2"
               imageSrc="./maxPayne.jpg"
-              projectName="Project 2"
+              projectName="Coming Soon"
             />
             <PortfolioCard
               id="3"
               imageSrc="./avengers.jpg"
-              projectName="Project 3"
+              projectName="Coming Soon"
             />
             <PortfolioCard
               id="2"
               imageSrc="./uncharted.jpg"
-              projectName="Project 4"
+              projectName="Coming Soon"
             />
             <PortfolioCard
               id="3"
               imageSrc="./wolverine.jpg"
-              projectName="Project 5"
+              projectName="Coming Soon"
             />
             <PortfolioCard
               id="1"
               imageSrc="./football.jpg"
-              projectName="Project 6"
+              projectName="Coming Soon"
             />
           </div>
         </div>
