d75802a268a0f5a51b3ef331b87e5d459b0f7072
Yashwanth Racherla <ignitingyash@gmail.com>
2022-07-02 19:08:53 +0530

    Improvised portfolio cards filtering

--- my-portfolio/src/components/Button.jsx
+++ my-portfolio/src/components/Button.jsx
@@ -1,26 +1,11 @@
 import React from "react";
 
-const Button = ({ text, id }) => {
-  const hideCards = (e) => {
-    const p_button = document.querySelectorAll(".portfolio-button");
-    const p_card = document.querySelectorAll(`.portfolio-card`);
-    const p_card_active = document.querySelectorAll(`.portfolio-card--${id}`);
-
-    p_button.forEach((button) => button.classList.remove("active"));
-    e.target.classList.add("active");
-
-    p_card.forEach((card) => card.classList.add("portfolio-card-not-active"));
-
-    p_card_active.forEach((card) =>
-      card.classList.remove("portfolio-card-not-active")
-    );
-  };
-
+const Button = ({ text, id, onButtonClick }) => {
   return (
     <button
       data-button-id={id}
       className="button portfolio-button"
-      onClick={(e) => hideCards(e)}
+      onClick={onButtonClick}
     >
       {text}
     </button>
--- my-portfolio/src/pages/Portfolio.jsx
+++ my-portfolio/src/pages/Portfolio.jsx
@@ -1,16 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../components/Button";
 import PortfolioCard from "../components/PortfolioCard";
 
 const Portfolio = () => {
-  const showAllButtons = () => {
-    document.querySelectorAll(".portfolio-card").forEach((card) => {
-      card.classList.remove("portfolio-card-not-active");
-    });
-    document
-      .querySelectorAll(".portfolio-button")
-      .forEach((button) => button.classList.remove("active"));
-  };
+  const [activeCategory, setActiveCategory] = useState(0);
+
   return (
     <>
       <div className="section portfolio-section">
@@ -19,48 +13,71 @@ const Portfolio = () => {
             <h2 className="section-title">My Personal Projects</h2>
           </div>
           <div className="flex justify-center gap-4 md:gap-6 lg:gap-8 my-10">
-            <Button id="1" text="Sports" />
-            <Button id="2" text="Games" />
-            <Button id="3" text="Movies" />
+            <Button
+              id="1"
+              text="Sports"
+              onButtonClick={() => setActiveCategory(1)}
+            />
+            <Button
+              id="2"
+              text="Games"
+              onButtonClick={() => setActiveCategory(2)}
+            />
+            <Button
+              id="3"
+              text="Movies"
+              onButtonClick={() => setActiveCategory(3)}
+            />
           </div>
-
           <div className="flex justify-center flex-wrap gap-10 mt-14">
-            <PortfolioCard
-              id="1"
-              imageSrc="./cricket.jpg"
-              projectName="My Cricket Website"
-              projectInfo="Being a cricket fan, I created a website which shows live cricket score updates."
-              navigateTo="https://cricbuzz.com"
-            />
-            <PortfolioCard
-              id="2"
-              imageSrc="./maxPayne.jpg"
-              projectName="Coming Soon"
-            />
-            <PortfolioCard
-              id="3"
-              imageSrc="./avengers.jpg"
-              projectName="Coming Soon"
-            />
-            <PortfolioCard
-              id="2"
-              imageSrc="./uncharted.jpg"
-              projectName="Coming Soon"
-            />
-            <PortfolioCard
-              id="3"
-              imageSrc="./wolverine.jpg"
-              projectName="Coming Soon"
-            />
-            <PortfolioCard
-              id="1"
-              imageSrc="./football.jpg"
-              projectName="Coming Soon"
-            />
+            {(activeCategory === 1 || activeCategory === 0) && (
+              <PortfolioCard
+                id="1"
+                imageSrc="./cricket.jpg"
+                projectName="My Cricket Website"
+                projectInfo="Being a cricket fan, I created a website which shows live cricket score updates."
+                navigateTo="https://cricbuzz.com"
+              />
+            )}
+            {(activeCategory === 2 || activeCategory === 0) && (
+              <PortfolioCard
+                id="2"
+                imageSrc="./maxPayne.jpg"
+                projectName="Coming Soon"
+              />
+            )}
+            {(activeCategory === 3 || activeCategory === 0) && (
+              <PortfolioCard
+                id="3"
+                imageSrc="./avengers.jpg"
+                projectName="Coming Soon"
+              />
+            )}
+            {(activeCategory === 2 || activeCategory === 0) && (
+              <PortfolioCard
+                id="2"
+                imageSrc="./uncharted.jpg"
+                projectName="Coming Soon"
+              />
+            )}
+            {(activeCategory === 3 || activeCategory === 0) && (
+              <PortfolioCard
+                id="3"
+                imageSrc="./wolverine.jpg"
+                projectName="Coming Soon"
+              />
+            )}
+            {(activeCategory === 1 || activeCategory === 0) && (
+              <PortfolioCard
+                id="1"
+                imageSrc="./football.jpg"
+                projectName="Coming Soon"
+              />
+            )}
           </div>
           <button
             className="button mt-8 block mx-auto"
-            onClick={() => showAllButtons()}
+            onClick={() => setActiveCategory(0)}
           >
             SHOW ALL PROJECTS
           </button>
