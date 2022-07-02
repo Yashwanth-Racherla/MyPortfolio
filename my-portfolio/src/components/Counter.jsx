5f5e9a8d547dc4834c5ddfc65987f4f981a3fe0d
Yashwanth Racherla <ignitingyash@gmail.com>
2022-07-02 17:09:30 +0530

    Counter Numbers changed

--- my-portfolio/src/components/Counter.jsx
+++ my-portfolio/src/components/Counter.jsx
@@ -17,7 +17,7 @@ const Counter = () => {
             const initialNum = parseInt(counterNum.innerText);
             const targetNum = parseInt(counterNum.dataset.number);
 
-            const speed = 100;
+            const speed = 200;
             const incrementNum = targetNum / speed;
 
             if (initialNum < targetNum) {
@@ -42,25 +42,25 @@ const Counter = () => {
     <div className="section counter-section">
       <div className="container text-white grid grid-cols-2 lg:grid-cols-4 gap-10 lg:py-6">
         <div className="text-center">
-          <span className="counter-numbers" data-number="400">
+          <span className="counter-numbers" data-number="4000">
             0
           </span>
           <p className="mt-4">Projects Completed</p>
         </div>
         <div className="text-center">
-          <span className="counter-numbers" data-number="200">
+          <span className="counter-numbers" data-number="2000">
             0
           </span>
           <p className="mt-4">Happy Clients</p>
         </div>
         <div className="text-center">
-          <span className="counter-numbers" data-number="500">
+          <span className="counter-numbers" data-number="5000">
             0
           </span>
           <p className="mt-4">Cups of Coffee</p>
         </div>
         <div className="text-center">
-          <span className="counter-numbers" data-number="100">
+          <span className="counter-numbers" data-number="1000">
             0
           </span>
           <p className="mt-4">Real Professionals</p>
